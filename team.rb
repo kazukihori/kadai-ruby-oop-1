65ac5e86aaf988074d9620680b842131a68f146d
kazuki.hori <git123.hoppy@gmail.com>
2021-12-15 02:06:58 +0000

    create team.rb

--- team.rb
+++ team.rb
@@ -1,56 +1,44 @@
+class Team 
+  attr_accessor :name,:win,:lose,:draw
+  
+  def  initialize(logo,up,down,keep)
+    self.name = logo
+    self.win = up
+    self.lose = down
+    self.draw = keep
+  end
 
-class Team
+  def calc_win_rate
+    self.win.to_f/(self.win.to_f + self.lose.to_f)
+  end
 
- attr_accessor :name, :win, :lose, :draw
- 
- def initialize(name,win,lose,draw)
-  self.name=name
-  self.win=win
-  self.lose=lose
-  self.draw=draw
- end
- 
- def clac_win_rate
-  self.win.to_f/(self.win.to_f+self.lose.to_f)
- end
- 
- def show_team_result
-  puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{clac_win_rate}です。"
- end
+def show_team_result
+  puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate
+  }です。"
 end
-  
-  team1=Team.new("Giants",67,45,8)
- 
-  
-  team2=Team.new("Tigers",60,53,7)
 
-  
-  team3=Team.new("Dragons",60,55,5)
- 
-  
-  team4=Team.new("BaySters",56,58,6)
-  
- 
-  team5=Team.new("Carp",52,56,12)
+end
 
+teamA = Team.new("Giants",67,45,8)
 
-  team6=Team.new("Swallows",41,69,10)
+teamB = Team.new("Tigers",60,53,7)
 
-  team1.show_team_result
-  
-  team2.show_team_result
-  
-  team3.show_team_result
-  
-  team4.show_team_result
-  
-  team5.show_team_result
-  
-  team6.show_team_result
+teamC = Team.new("Dragons",60,55,5)
 
-  
+teamD = Team.new("BayStars",56,58,6)
 
-    
-  
-  
-   
+teamE = Team.new("Carp",52,56,12)
+
+teamF = Team.new("Swallows",41,69,10)
+
+teamA.show_team_result
+
+teamB.show_team_result
+
+teamC.show_team_result
+
+teamD.show_team_result
+
+teamE.show_team_result
+
+teamF.show_team_result
